aa5ed2db7ccff2dc0806efa484f1a72de2b5ef0b
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-06 14:21:50 +0200

    Using the home component.

--- bases/frontend/src/app/frontend/views.cljs
+++ bases/frontend/src/app/frontend/views.cljs
@@ -3,12 +3,15 @@
             [re-frame.core :as re-frame]
             [app.frontend.events :as events]
             [app.frontend.subs :as subs]
+            ["/pages/Home$default" :as home]
             ["/components/landing/Survey$default" :as survey]))
 
 (js/console.log survey)
 
+(def home-component (r/adapt-react-class home))
 (def survey-component (r/adapt-react-class survey))
 
 (defn app []
   [:<>
-   [survey-component {:isLoading false}]])
+   [home-component {:isLoggedIn false :user {}}
+    [survey-component {:isLoading false}]]])
