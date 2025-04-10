4d32783adb1e5a5e979421ed8d5d71d0addc8174
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-10 15:55:52 +0200

    Added more necessary properties.

--- bases/frontend/src/app/frontend/views.cljs
+++ bases/frontend/src/app/frontend/views.cljs
@@ -9,20 +9,25 @@
             ["/pages/home$default" :as home]
             ["/components/landing/Survey$default" :as survey]))
 
-(defn app []
+#_(defn app []
   [:<>
    [user-view/login-component]])
 
 (def home-component (r/adapt-react-class home))
 (def survey-component (r/adapt-react-class survey))
 
-#_(defn app []
+(defn app []
   [:<>
    [home-component {:isLoggedIn false :user {}}
     [survey-component {:isLoading false 
                        :questions @(re-frame/subscribe [::survey-subs/questions])
                        :currentQuestionIndex @(re-frame/subscribe [::survey-subs/current-question-index])
+                       :currentQuestionResponse @(re-frame/subscribe [::survey-subs/current-question-response])
                        :showEmailForm @(re-frame/subscribe [::survey-subs/show-email-form])
                        :handleAnswerSelection #(re-frame/dispatch [::survey-events/answer-question %])
                        :handleNext #(re-frame/dispatch [::survey-events/next-question])
-                       :handlePrevious #(re-frame/dispatch [::survey-events/previous-question])}]]])
+                       :handlePrevious #(re-frame/dispatch [::survey-events/previous-question])
+                       :isEmailFormPending @(re-frame/subscribe [::survey-subs/email-form-pending])
+                       :email @(re-frame/subscribe [::survey-subs/form :email])
+                       :onChangeEmail #(re-frame/dispatch [::survey-events/update-email-form :email (-> % .-target .-value)])
+                       :handleSubmit #(re-frame/dispatch [::survey-events/save-survey])}]]])
