ee6463b24f89266d0fcbdf75e16d2afe741e5db2
Yunior <yunior.alayo@gmail.com>
2026-03-13 12:19:19 +0100

    Made the relationship optional.

--- projects/cloudflare/migrations/0005_tenants.sql
+++ projects/cloudflare/migrations/0005_tenants.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `props_tenants` (
     `id`           INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    `apartment_id` INTEGER NOT NULL,
+    `apartment_id` INTEGER,
     `name`         TEXT NOT NULL,
     `email`        TEXT,
     `phone`        TEXT,
