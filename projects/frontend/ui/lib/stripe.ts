f3c7fddb55a8389d5c6303bb87f3e2621c704ad2
Yunior <yunior.alayo@gmail.com>
2026-03-13 15:08:26 +0100

    Updated the stripe config.

--- projects/frontend/ui/lib/stripe.ts
+++ projects/frontend/ui/lib/stripe.ts
@@ -3,7 +3,7 @@ import { loadStripe, Stripe } from "@stripe/stripe-js";
 // Public keys are safe to include client-side.
 // Use test key in dev, live key in prod.
 const STRIPE_PUBLIC_KEY_DEV  = "pk_test_51TAVxzPPxtRLtqH5dh1XbriAke1cBFOIbEVcfbNqE4lah9JkVFc5oZsq0yxvT1vtddLy5iIIOOsF2EvcFBcknCdR00gpJpHmoV";
-const STRIPE_PUBLIC_KEY_PROD = "pk_test_51TAVxzPPxtRLtqH5dh1XbriAke1cBFOIbEVcfbNqE4lah9JkVFc5oZsq0yxvT1vtddLy5iIIOOsF2EvcFBcknCdR00gpJpHmoV";
+const STRIPE_PUBLIC_KEY_PROD = "pk_live_51JPU6JACoBzNYHXtV5BAADS6fdG16WNxQDIW8FLJBNaXBciY800hIglZ1UwODdwb2jCNKpZVNM5Lzeh3boyXA3E800U33kOH5s";
 
 function getPublicKey(): string {
   const host = typeof window !== "undefined" ? window.location.host : "";
